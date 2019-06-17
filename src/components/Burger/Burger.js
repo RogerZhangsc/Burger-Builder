3698b2d1f408f4d19057fcf918b36d69ee5985e6
Shenchao Zhang <shenchao.zhang@exciting212.sd.tusimple.ai>
2019-06-17 15:40:04 -0700

    164

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    return 
-}
+    let transformerIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    // console.log(transformerIngredients)
+    if (transformerIngredients.length === 0) {
+        // alert('Please add ingredients.');
+        transformerIngredients = <p>Please add ingredients</p>;
+    };
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type='bread-top' />
+            {transformerIngredients}
+            <BurgerIngredient type='bread-bottom' />
+        </div>
+
+    );
+};
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,90 @@
 import React, { Component, Fragment } from 'react';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7,
+}
 class BurgerBuilder extends Component {
 
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        },
+        totalPrice: 4,
+        puchasable: false,
+    };
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: newPrice,
+        }, this.updatePuchasable);
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount === 0) {
+            return;
+        } else {
+            const updatedCount = oldCount - 1;
+            const updatedIngredients = {
+                ...this.state.ingredients
+            };
+            updatedIngredients[type] = updatedCount;
+            const priceSubstraction = INGREDIENT_PRICES[type]
+            const oldPrice = this.state.totalPrice;
+            const newPrice = oldPrice - priceSubstraction;
+            this.setState({
+                ingredients: updatedIngredients,
+                totalPrice: newPrice,
+            }, this.updatePuchasable);
+        }
+    }
+
+    updatePuchasable = () => {
+        const ingredients = {...this.state.ingredients}
+        console.log(ingredients)
+        const puchasable = Object.values(ingredients)
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0) ;
+        console.log(puchasable);
+        this.setState({ puchasable: puchasable })
+    };
+
     render() {
+        const disabledInfo = { ...this.state.ingredients };
+        // console.log(Object.values(this.state.ingredients));
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        };
         return (
             <Fragment>
-                <div>Burger</div>
-                <div>Burger Controls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice}
+                    puchasable={this.state.puchasable} />
             </Fragment>
-        )
+        );
     }
 }
 
