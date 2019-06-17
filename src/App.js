5ec19f4eeba9e3749f2dc504223c3f8a484e4c69
Shenchao Zhang <shenchao.zhang@exciting212.sd.tusimple.ai>
2019-06-17 10:46:16 -0700

    burger

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Ingredient from './components/Burger/BurgerIngredient/BurgerIngredient';
 
 function App() {
   return (
     <div>
       <Layout>
         <BurgerBuilder />
+        <Ingredient type='meat'></Ingredient>
       </Layout>
     </div>
   );
